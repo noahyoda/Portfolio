02fef021ed41238863aed47f16987414bb737d55
Noah <noahyoda1@gmail.com>
2023-08-23 10:17:57 -0600

    fixed paragraph overflow bug

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,32 +1,27 @@
 // src/components/Experience.js
 import './Experience.css';
 
-function ExperienceItem(props) {
-    return (
-        <div className="ExperienceItem">
-            <div className="ExperienceItemColumn Date-Column">
-                <h3 className='Left Date'>{props.time}</h3>
-            </div>
-            <div className="ExperienceItemColumn Exp-Column">
-                <h3 className='Right Title'>{props.title}</h3>
-                <h3 className='Right Company'>{props.company}</h3>
-                <p className='Right Description'>{props.description}</p>
-            </div>
-        </div>
-    )
-}
-
 export default function Experience() {
     let exp = {
-        title: "Software Engineer",
-        company: "Google",
-        time: "2019 - Present",
-        description: "I work on the Google Search team, where I help build the world's most popular search engine."
+        title: "Software Engineer Intern",
+        company: "General Atomics Aeronautical Systems, Inc.",
+        time: "June 2023 - Aug 2023",
+        description: "Developed multiple inhouse software development tools with a team that involved languages such as C, C++, and Python. Implemented the use of low level data manipulation (endianness, signage, etc), multithreading, memory locks, networking (TCP, UDP, Multicast), GUI work in C++, and data visualization. More info available upon request."
     }
 
     return (
-        <div className="ExperienceView">
-            <ExperienceItem time={exp.time} title={exp.title} company={exp.company} description={exp.description} />
+        <div className="ExperienceItem">
+            <div className="Date-Column">
+                <h3 className='Date'>{exp.time}</h3>
+            </div>
+            <div className="Exp-Column">
+                <h3 className='Title'>{exp.title}</h3>
+                <h3 className='Company'>{exp.company}</h3>
+                <p className='Description'>
+                    {exp.description}
+                </p>
+                <h4 className='Right SubHeader'>Technologies Used</h4>
+            </div>
         </div>
     )
 }
