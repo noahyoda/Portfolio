9ab320a53247c6d701012d18f8acfc0b62fbff04
Noah <noahyoda1@gmail.com>
2023-08-28 15:13:13 -0600

    added link to ga-asi

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,7 +5,9 @@ function SingleExperience(props) {
     return (
         <div className="Exp-Column">
             <h3 className='Title'>{props.title}</h3>
-            <h3 className='SubTitle'>{props.company}</h3>
+            <a className='Link' href={props.link}>
+                <h3 className='SubTitle'>{props.company}</h3>
+            </a>
             <p className='Description'>
                 {props.description}
             </p>
@@ -25,6 +27,7 @@ export default function Experience() {
     let exp = [{
         title: "Software Engineer Intern",
         company: "General Atomics Aeronautical Systems, Inc.",
+        link: "https://www.ga-asi.com/",
         time: "June 2023 - Aug 2023",
         description: "Developed Gitlab CI pipeline(s) to automate and expedite the process of creating new virtual machines from an in-house Linux distribution. Also developed an application to modernize internal and external device communications through an in-house network solution.",
         accomplishments: ["Created a Gitlab pipeline that replaced the need for experts to manually build operating systems and install it on virtual machines for developers increasing overall developer efficiency.",
@@ -33,6 +36,7 @@ export default function Experience() {
     {
         title: "Software Engineer Intern",
         company: "General Atomics Aeronautical Systems, Inc.",
+        link: "https://www.ga-asi.com/",
         time: "June 2022 - Aug 2022",
         description: "Developed multiple inhouse software development tools with a team that involved languages such as C, C++, and Python. Implemented the use of low level data manipulation (endianness, signage, etc), multithreading, memory locks, networking (TCP, UDP, Multicast), GUI work in C++, and data visualization. More info available upon request.",
         accomplishments: ["Created an application that replaced legacy software to load software onto embedded devices with substantial speed and throughput increases.",
@@ -47,7 +51,7 @@ export default function Experience() {
                         <div className="Date-Column">
                             <h3 className='Date'>{item.time}</h3>
                         </div>
-                        <SingleExperience title={item.title} company={item.company} time={item.time} description={item.description} accomplishments={item.accomplishments}/>
+                        <SingleExperience title={item.title} company={item.company} link={item.link} time={item.time} description={item.description} accomplishments={item.accomplishments}/>
                     </div>
                 )
             }
